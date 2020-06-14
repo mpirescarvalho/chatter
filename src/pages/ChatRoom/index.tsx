109c277096691cabee975e8384f5dad292bbd14d
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-06-14 01:28:35 -0300

    visual design of chat room page

--- src/pages/ChatRoom/index.tsx
+++ src/pages/ChatRoom/index.tsx
@@ -1,10 +1,191 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
-import io from "socket.io-client";
+import { FiArrowLeft, FiSend } from "react-icons/fi";
+import styled from "styled-components";
+// import io from "socket.io-client";
 
 import NotFound from "../../components/NotFound";
 
 import api from "../../services/api";
+
+const Container = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Box = styled.div`
+	width: 1200px;
+	height: 600px;
+	background-color: #fff;
+	border: 1px solid #ddd;
+	border-radius: 8px;
+	display: flex;
+	flex-direction: column;
+`;
+
+const TitleBar = styled.div`
+	width: 100%;
+	height: 40px;
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	align-items: center;
+	flex: 0 1 auto;
+	font-size: 15px;
+	color: #151515;
+	border-bottom: 1px solid #ddd;
+
+	span {
+		color: #34cb79;
+		padding: 0 10px;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		font-size: 17px;
+		transition: all 0.5s;
+		margin-right: 5px;
+
+		:hover {
+			background-color: #ddd;
+		}
+	}
+`;
+
+const RoomBox = styled.div`
+	flex: 1 1 auto;
+	display: flex;
+	flex-direction: row;
+`;
+
+const PeopleContainer = styled.ul`
+	height: 100%;
+	flex: 0 1 220px;
+`;
+
+const Person = styled.li`
+	width: 100%;
+	height: 60px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: flex-start;
+	font-size: 13px;
+	border-bottom: 1px solid #ddd;
+`;
+
+interface CircleProps {
+	color: string;
+}
+
+const Circle = styled.div<CircleProps>`
+	border-radius: 200px;
+	height: 35px;
+	width: 35px;
+	margin: 0 15px;
+	background-color: ${(props) => props.color};
+`;
+
+const Chat = styled.div`
+	height: 100%;
+	flex: 1 1 auto;
+	background-color: #ddd;
+	display: flex;
+	flex-direction: column;
+`;
+
+const ContainerMessages = styled.ul`
+	width: 100%;
+	flex: 1 1 auto;
+	padding: 0 15px;
+	padding-bottom: 5px;
+	display: flex;
+	flex-direction: column-reverse;
+`;
+
+interface ContainerMessageProps {
+	owner: boolean;
+}
+
+const ContainerMessage = styled.li<ContainerMessageProps>`
+	width: 100%;
+	list-style-type: none;
+	display: flex;
+	flex-direction: row;
+	margin-top: 30px;
+	justify-content: ${(props) => (props.owner ? "flex-end" : "flex-start")};
+`;
+
+interface MessageSenderProps {
+	color: string;
+}
+
+const MessageBox = styled.div`
+	background-color: #fff;
+	max-width: 350px;
+	border-radius: 8px;
+	overflow-x: hidden;
+	padding: 10px;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: flex-start;
+`;
+
+const MessageSender = styled.div<MessageSenderProps>`
+	color: ${(props) => props.color};
+	border-bottom: 1px solid #ddd;
+	margin-bottom: 5px;
+`;
+
+const MessageContent = styled.div`
+	font-size: 13px;
+`;
+
+const ContainerInput = styled.form`
+	width: 100%;
+	flex: 0 1 60px;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	padding: 8px 15px;
+`;
+
+const Input = styled.input`
+	flex: 1 1 auto;
+	height: 100%;
+	padding: 0 15px;
+	border-top-left-radius: 100px;
+	border-bottom-left-radius: 100px;
+	border: 1px solid #34cb79;
+	font-size: 12px;
+`;
+
+const Submit = styled.button`
+	flex: 0 1 50px;
+	height: 100%;
+	border-top-right-radius: 100px;
+	border-bottom-right-radius: 100px;
+	border: none;
+	background-color: #34cb79;
+	color: #fff;
+	font-size: 24px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding-right: 7px;
+	transition: all 0.5s;
+
+	:hover {
+		background-color: #fff;
+		color: #34cb79;
+		border: 1px solid #34cb79;
+		border-left: none;
+	}
+`;
 
 interface Message {
 	sender_id: string;
@@ -20,33 +201,47 @@ interface Room {
 	}[];
 }
 
-// const room: Room = {
-// 	id: "1",
-// 	name: "Avenger's Room",
-// 	people: [
-// 		{
-// 			id: "1",
-// 			nickname: "Marcelo",
-// 		},
-// 		{
-// 			id: "2",
-// 			nickname: "João",
-// 		},
-// 		{
-// 			id: "3",
-// 			nickname: "Maria",
-// 		},
-// 	],
-// };
+const colors = [
+	"#EB5757",
+	"#F2994A",
+	"#F2C94C",
+	"#219653",
+	"#2F80ED",
+	"#9B51E0",
+	"#795548",
+	"#607d8b",
+];
+
+interface IHash {
+	[key: string]: any;
+}
 
 //TODO: Implementar loading spinner
 const ChatRoom = () => {
 	const [loading, setLoading] = useState(true);
 	const [room, setRoom] = useState<Room>();
 	const [nickname, setNickname] = useState<String | undefined>();
+	const [peopleColors, setPeopleColors] = useState<IHash>({});
 
 	const { room_id } = useParams();
 	const { search } = useLocation();
+
+	//TODO: get id from server
+	const myID = 1;
+
+	useEffect(() => {
+		if (room) {
+			let _peopleColors: IHash = {};
+			room.people.forEach(
+				(person) =>
+					(_peopleColors[`${person.id}`] =
+						colors[Math.floor(Math.random() * colors.length)])
+			);
+			setPeopleColors(_peopleColors);
+		} else {
+			setPeopleColors({});
+		}
+	}, [room]);
 
 	useEffect(() => {
 		const match = search.match(/nickname=(.+)/);
@@ -61,7 +256,6 @@ const ChatRoom = () => {
 			api
 				.get(`/rooms/${room_id}`)
 				.then((response) => {
-					console.log(response.data);
 					setRoom(response.data);
 					setLoading(false);
 				})
@@ -89,7 +283,62 @@ const ChatRoom = () => {
 
 	if (!room || !nickname) return <NotFound />;
 
-	return <h1>abc: {room.name}</h1>;
+	return (
+		<Container>
+			<Box>
+				<TitleBar>
+					<span>
+						<FiArrowLeft />
+					</span>
+					<strong>{room.name}</strong>
+				</TitleBar>
+				<RoomBox>
+					<PeopleContainer>
+						{room.people.map((person, index) => (
+							<Person key={index}>
+								<Circle color={peopleColors[`${person.id}`]} />
+								<span>{person.nickname}</span>
+							</Person>
+						))}
+					</PeopleContainer>
+					<Chat>
+						<ContainerMessages>
+							<ContainerMessage owner={true}>
+								<MessageBox>
+									<MessageSender color={peopleColors["1"]}>
+										<strong>Marcelo Carvalo</strong>
+									</MessageSender>
+									<MessageContent>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+										auctor imperdiet libero non laoreet. Mauris vel mauris nec eros
+										pretium posuere id et ipsum.
+									</MessageContent>
+								</MessageBox>
+							</ContainerMessage>
+							<ContainerMessage owner={false}>
+								<MessageBox>
+									<MessageSender color={peopleColors["1"]}>
+										<strong>Marcelo Carvalo</strong>
+									</MessageSender>
+									<MessageContent>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+										auctor imperdiet libero non laoreet. Mauris vel mauris nec eros
+										pretium posuere id et ipsum.
+									</MessageContent>
+								</MessageBox>
+							</ContainerMessage>
+						</ContainerMessages>
+						<ContainerInput>
+							<Input />
+							<Submit type="submit">
+								<FiSend />
+							</Submit>
+						</ContainerInput>
+					</Chat>
+				</RoomBox>
+			</Box>
+		</Container>
+	);
 };
 
 export default ChatRoom;
